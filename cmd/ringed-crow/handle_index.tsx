4f285e3292de1e60ab8c279f7da665265bc78b8f
Rahim <kristopherab@gmail.com>
2023-10-04 21:11:39 +0100

    Update handle_index.tsx

--- cmd/ringed-crow/handle_index.tsx
+++ cmd/ringed-crow/handle_index.tsx
@@ -21,7 +21,7 @@ export function handleIndex(): Handler {
         const { q: name } = parseDto(c.query);
 
         return (
-            <Base head={{ title: "Home" }}>
+            <Base head={{ title: "Welcome" }}>
                 <header>
                     <nav>
                         <ul>
