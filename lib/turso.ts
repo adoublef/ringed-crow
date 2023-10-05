2375e598358d41d4f88ec1fc535978b44b8d1253
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-05 08:24:51 +0100

    update turso lib with ping and middleware

---
+++ lib/turso.ts
@@ -0,0 +1,27 @@
+import { Client } from "@libsql/client";
+import Elysia from "elysia";
+
+/**
+ * Turso will decorate the router with a database connection
+ */
+export function turso(c: Client) {
+    const app = new Elysia()
+        .decorate("db", c);
+    return app;
+}
+
+/**
+ * Ping will ping the database to check that all is good.
+ * 
+ * Throws error if it failed the request.
+ */
+export async function ping(c: Client): Promise<true> {
+    // this will currently only ping `SELECT 42`
+    // TODO -- ping for specific tables in the table
+    const ok = (await c.execute("SELECT 42")).rows.length !== 0;
+    if (!ok) {
+        throw new ReferenceError("error pinging the database")
+    }
+
+    return ok
+}
