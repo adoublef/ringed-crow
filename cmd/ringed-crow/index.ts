3466e44787c22737e5cdf83681b590fd74df26fc
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-05 09:40:35 +0100

    add db client to index

--- cmd/ringed-crow/index.ts
+++ cmd/ringed-crow/index.ts
@@ -3,15 +3,25 @@ import { handleIndex } from "./handle_index";
 import { serve } from "~/lib/serve";
 import { html } from "@elysiajs/html";
 import { handleAbout } from "./handle_about";
+import { ping, turso } from "~/lib/turso";
+import { createClient } from "@libsql/client";
+import { env } from "~/lib/env";
 
 if (
     import.meta.main
 ) {
+    const db = createClient({ url: env("DATABASE_URL") });
+    await ping(db);
+
     const app = new Elysia()
-    .use(html());
+        .use(turso(db))
+        .use(html());
 
     app.get("/", handleIndex());
-    app.get("/about", handleAbout())
+    app.get("/about", handleAbout());
 
-    await serve(app)
+    await serve(app);
+
+    // TODO -- handle graceful shutdown
+    // required to sync changes to Tursogs
 }
