526a87ff7405e2dec2de88b5d61c52f413ddee20
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-05 09:56:13 +0100

    use client to make a sql request

--- cmd/ringed-crow/handle_index.tsx
+++ cmd/ringed-crow/handle_index.tsx
@@ -1,12 +1,14 @@
 import { Html } from "@elysiajs/html";
+import { Client } from "@libsql/client";
 import { Handler } from "elysia";
-import { Output, maxBytes, object, optional, parse, string } from "valibot";
+import { Output, maxBytes, object, optional, parse, string, transform } from "valibot";
 import { Base } from "~/jsx/dom/base";
+import { Decorator as DTurso } from "~/lib/turso";
 
-export function handleIndex(): Handler {
-    const dto = object({
+export function handleIndex(): Handler<any, DTurso> {
+    const dto = transform(object({
         q: optional(string([maxBytes(8)]))
-    });
+    }), ({ q }) => ({ q: q ?? "world" }));
 
     const parseDto = (data: unknown): Output<typeof dto> => {
         try {
@@ -17,8 +19,16 @@ export function handleIndex(): Handler {
         }
     };
 
-    return c => {
-        const { q: name } = parseDto(c.query);
+    const simulate =async (c: Client, name: string)=>{
+        const rs = ((await c.execute({
+            sql: "SELECT ?", args: [name]
+        })).rows.at(0)!);
+
+        return (rs)[0] as string
+    }
+
+    return async (c) => {
+        const { q } = parseDto(c.query);
 
         return (
             <Base head={{ title: "Welcome" }}>
@@ -37,10 +47,14 @@ export function handleIndex(): Handler {
                 <main>
                     <hgroup>
                         <h1>
-                            Hello, {name ?? "world"}!
+                            Hello, {await simulate(c.db, q)}!
                         </h1>
                         <h2>Still under construction 👷🏿</h2>
                     </hgroup>
+                    <p>
+                        For the meantime you can set your name via the header using the <code>q</code> parameter.
+                        The maximum value for this is 20. You will be greeted in the header
+                    </p>
                 </main>
                 <footer>
                     <small>Powered by <a hx-boost={false} href="https://bun.sh">Bun</a></small>
