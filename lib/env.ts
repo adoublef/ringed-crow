82201efdcd7c5ef9395c6f82e3ab3efab25fe612
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-06 02:26:54 +0100

    update env

--- lib/env.ts
+++ lib/env.ts
@@ -1,10 +1,14 @@
+function unquote(value: string) {
+    return value.replace(/^['"]|['"]$/g, "");
+}
+
 /**
  * Env is a helper to get environment variable 
  */
 export function env(key: string): string {
-    const value = process.env[key]
-    if (!value) {
-        throw new ReferenceError(`error finding env variable for ${key}`)
+    const value = process.env[key];
+    if (value === undefined) {
+        throw new ReferenceError(`"${key}" environment variable must be set`);
     }
-    return value
+    return unquote(value);
 }
